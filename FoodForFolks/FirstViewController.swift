f1dbaef2144418929c304d76a636858e85857506
Cory Rooker <clrooker@corys-macbook-pro.local>
2019-03-05 14:18:46 -0500

    Initial Commit

---
+++ FoodForFolks/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  FoodForFolks
+//
+//  Created by Cory L. Rooker on 3/5/19.
+//  Copyright © 2019 Cory L. Rooker. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ FoodForFolks/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  FoodForFolks
+//
+//  Created by Cory L. Rooker on 3/5/19.
+//  Copyright © 2019 Cory L. Rooker. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
