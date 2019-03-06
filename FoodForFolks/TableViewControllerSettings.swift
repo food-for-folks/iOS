240bfcd22bf6ec1723f5221697365cba926428c3
crooker <crooker@uncc.edu>
2019-03-05 20:41:31 -0500

    Changed settings to a TableViewController to get static cells and added basic layout to controller. Updates to the message layout as well.

---
+++ FoodForFolks/TableViewControllerSettings.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewControllerSettings.swift
+//  FoodForFolks
+//
+//  Created by Cory L. Rooker on 3/5/19.
+//  Copyright © 2019 Cory L. Rooker. All rights reserved.
+//
+
+import UIKit
+
+class TableViewControllerSettings: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+}
--- FoodForFolks/ViewControllerMessages.swift
+++ FoodForFolks/ViewControllerMessages.swift
@@ -15,16 +15,30 @@ class ViewControllerMessages: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
 
-    /*
-    // MARK: - Navigation
+}
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+
+extension ViewControllerMessages: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MessagesCell", for: indexPath)
+        
+        return cell
+    }
+}
 
+extension ViewControllerMessages: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, accessoryButtonTappedForRowWith indexPath: IndexPath) {
+        
+    }
+    
 }
